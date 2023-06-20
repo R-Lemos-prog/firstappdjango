0d6f2c76f0d3722ed7e77502b66f4f839b6917b5
Raiene Lemos <raienelemos@outlook.com>
2023-06-20 19:19:29 -0300

    Monitorando Erros com Sentry e debug_toolbar

--- project/core/views.py
+++ project/core/views.py
@@ -2,4 +2,5 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('First app django')
+    raise ValueError()
+    return HttpResponse('<html><body><h1>First app django</h1><p>A palavra paciência procede do latim pati, que significa padecer. A virtude da paciência é, de fato, “a arte de padecer”. Quando está vitalizada pela graça do Espírito Santo, pode-se definir como “a arte se sofrer com fé, esperança e amor”, sobretudo com amor.</p></body></html>', content_type='text/html')
--- project/settings.py
+++ project/settings.py
@@ -14,6 +14,9 @@ import os
 from pathlib import Path
 from decouple import config, Csv
 from dj_database_url import parse as db_url
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,6 +78,14 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'project.wsgi.application'
+
+# Configuração Django Debug Toolbar
+
+INTERNAL_IPS = config('INTERNAL_IPS', cast=Csv(), default='127.0.0.1')
+
+if DEBUG:
+    INSTALLED_APPS.append("debug_toolbar")
+    MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")
 
 
 # Database
@@ -165,4 +176,11 @@ if AWS_ACCESS_KEY_ID:
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTEDS_ORIGENS = ["https://firstappdjango.fly.dev"]
+# CSRF_TRUSTEDS_ORIGENS = ["https://firstappdjango.fly.dev"]
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+SENTRY_DSN = config('SENTRY_DSN', default=None)
+
+if SENTRY_DSN:
+    sentry_sdk.init(dsn=SENTRY_DSN, integrations=[DjangoIntegration(),],)
