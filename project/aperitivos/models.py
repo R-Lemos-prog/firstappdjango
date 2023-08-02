5876b0b0c5a7b69dff18a79798d4c80a3b647c4f
Raiene Lemos <raienelemos@outlook.com>
2023-08-02 19:23:12 -0300

    personalização do admin aperitivos

--- project/aperitivos/models.py
+++ project/aperitivos/models.py
@@ -3,10 +3,15 @@ from django.db import models
 
 
 class Video(models.Model):
-    slug = models.CharField(max_length=32)
-    titulo = models.SlugField(max_length=32)
+    titulo = models.CharField(max_length=32)
+    slug = models.SlugField(max_length=32)
     vimeo_id = models.CharField(max_length=32)
     creation = models.DateTimeField(auto_now_add=True)
 
     def get_absolute_url(self):
         return reverse('aperitivos:video', args=(self.slug,))
+
+    # Gera a alteração de exibição: Ex. "O video “Video: Aperitivo Motivação” foi alterado com sucesso."
+
+    def __str__(self):
+        return f'Video: {self.titulo}'
